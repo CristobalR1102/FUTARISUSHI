50da4e8eb5517c7af3320ac019af1f8988f374fe
CristobalR1102 <cristobalrenato.2005@gmail.com>
2026-05-16 18:14:10 -0400

    fix: extension logo en minusculas

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import logo from "../assets/LOGO_BAZZI_CHICKEN.JPEG"
+import logo from "../assets/LOGO_BAZZI_CHICKEN.jpg"
 
 export default function Header({ activeCategory, setActiveCategory, cartCount, onInfo, onEspecial, menu }) {
   return (
