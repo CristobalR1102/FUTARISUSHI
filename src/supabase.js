7b8c862bc0318ff1df70006f28d5c2362d5047d0
CristobalR1102 <cristobalrenato.2005@gmail.com>
2026-05-29 21:49:28 -0400

    creacion del .env

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = "https://gnenxuwzljdguzftflov.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImduZW54dXd6bGpkZ3V6ZnRmbG92Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2Nzg2MDEsImV4cCI6MjA5NDI1NDYwMX0.toV6CBZYxpJgDkWQy48BFA9GRnQn4rmgWctzXxxf16M"
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
